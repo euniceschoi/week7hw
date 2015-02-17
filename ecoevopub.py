3f04707582f0d104c612d4079c0b9ddeb86bd912
Eunice Choi <euniceschoi93@gmail.com>
2015-02-17 17:50:43 +0000

    ecoevopub

---
+++ ecoevopub.py
@@ -0,0 +1,13 @@
+#download 800 pages
+curl 'https://www.eeb.ucla.edu/seminars.php?id=[001-800]'> [1-800].html
+
+#get names and dates
+from bs4 import BeautifulSoup
+import os, os.path
+seminarlist= os.listdir("seminars")
+for seminar in seminarlist:
+	soup= BeautifulSoup(open("seminars/"+seminar))
+	name_pane=soup.find(class_="section")
+	date=name_pane.find("h4")
+	name=name_pane.find("p")
+	print date.string, name.string
